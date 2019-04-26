b9487fc88d5a9e7bbe2cf327b12d539ffef6d699
jayaimzzz <jameshorton@gmail.com>
2019-04-26 16:39:14 -0400

    having trouble with the keys being bytes

---
+++ key_generator.py
@@ -0,0 +1,58 @@
+__author__ = 'jayaimzzz'
+
+import argparse
+import hashlib, binascii
+import random
+
+password = b'letmein'
+random_num_min= 1000
+random_num_max= 1005
+
+def generate_random_key():
+    salt = bytes(random.randint(random_num_min,random_num_max))
+    dk = generate_key(salt)
+    return dk
+
+def generate_key(salt):
+    
+    key = hashlib.pbkdf2_hmac('sha256',password,salt, 100000)
+    return key.decode('utf-8')
+    
+def validate_key(key):
+    isValid = False
+    for i in range(random_num_min,random_num_max):
+        salt = bytes(i)
+        dk = generate_key(salt)
+        print("key: " + key)
+        print("dk: " + dk)
+        if dk == key:
+            isValid = True
+            break
+    return isValid
+        
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description="Generate a unique key or validate a key")
+    parser.add_argument('-g','--generatekey', help='generates one key', action='store_true')
+    parser.add_argument('-v','--validatekey', help='validates key provided', action='store_true')
+    parser.add_argument('-key', help='The key needing validation', type=str, required=False)
+    return parser
+
+if __name__ == '__main__':
+    parser = create_parser()
+    args = parser.parse_args()
+    if args.validatekey and not args.key:
+        print("Key must be provided when validating. Example: -v -key keyToValidate")
+        exit()
+    if args.generatekey:
+        print(generate_random_key())
+        exit()
+    if args.validatekey and args.key:
+        isValid = validate_key(args.key)
+        if isValid:
+            print("Key is Valid")
+        else:
+            print("Key is NOT Valid")
+    
+    
